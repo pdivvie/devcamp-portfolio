7bf238bceaf906b4e9aa050f6d4f9eac7caa406e
pierre <pdivvie@outlook.com>
2018-11-06 14:52:30 +0200

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @porfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
