60cf463731b3190d49bbc86d99d3acdfc2c40995
pierre <pdivvie@outlook.com>
2019-07-02 16:58:56 +0200

    Updated carrierwave.rb to work with credentials file instead of ENV file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+  config.aws_bucket = Rails.application.credentials.production[:aws][:s3_bucket_name] # for AWS-side bucket access permissions config, see section below
   config.aws_acl    = 'private'
 
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
@@ -10,9 +10,9 @@ CarrierWave.configure do |config|
   }
 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION'), # Required
+    access_key_id:     Rails.application.credentials.production[:aws][:access_key_id],
+    secret_access_key: Rails.application.credentials.production[:aws][:secret_access_key],
+    region:            Rails.application.credentials.production[:aws][:region], # Required
     stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
   }
 end
