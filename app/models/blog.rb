4e7e06ec77ec8603cac97548fdf7e317b69630af
pdivvie <pdivvie@outlook.com>
2019-07-30 13:21:00 +0200

    Implemented custom scope for recent blogs and hid draft posts from non site_admins as well as adding topics to the new blog form

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-  enum status: { draft: 0, published: 1} 
+  enum status: { draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
 
@@ -15,5 +15,9 @@ class Blog < ApplicationRecord
 
   def self.featured_blogs
     limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
   end
 end
